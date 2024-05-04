a42cbcff3728d46657cfb862916be66bfd55172c
mdeandwibekti <mdeandwibekti@gmail.com>
2024-05-04 13:40:18 +0700

    ke 17

--- prktikum double linked/prktikum double linked.cpp
+++ prktikum double linked/prktikum double linked.cpp
@@ -183,6 +183,14 @@ void searchData()
 		cout << "\nList is empty" << endl;
 	}
 
+	Node* prev, * curr;
+	prev = curr = NULL;
+	cout << "\nEnter the roll number of the student whose record you want to search: ";
+	int num;
+	cin >> num;
+	if (search(num, &prev, &curr) == false)
+		cout << "\nRecord not found" << endl;
+
 
 
 
