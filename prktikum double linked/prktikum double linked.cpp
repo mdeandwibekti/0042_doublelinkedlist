2ace7014fb378ec320518aa95a95d5a6c77df757
mdeandwibekti <mdeandwibekti@gmail.com>
2024-05-04 12:22:24 +0700

    if

--- prktikum double linked/prktikum double linked.cpp
+++ prktikum double linked/prktikum double linked.cpp
@@ -20,6 +20,11 @@ void addNode()
 	cout << "\nEnter the name of the student: ";
 	cin >> newNode->name; // Assign value to the data field of the new node
 
+	// Insert the new node in the list
+	if (START == NULL || newNode->noMhs <= START->noMhs)
+	{ // step 2: insert the new node at the beginning
+
+
 
 
 
